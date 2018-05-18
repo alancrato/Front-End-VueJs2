45b1b102dcd8277b29c63f1109d3009b9a9db165
Alan Rodrigues <alancrato@hotmail.com>
2018-05-18 10:48:34 -0300

    trabalhando com whachtes

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ new Vue({
 
   data: {
 
+    message: 'message',
+
     title: 'Contas a Pagar',
 
     menus: [
@@ -13,16 +15,16 @@ new Vue({
         {id: 1, name: 'Criar Contas'}
     ],
 
-    activedView: 0,
+    activedView: 1,
 
     bills: [
         {date_due: '20/08/2018', name: 'Conta de Luz', value: 70.99, done: 1},
-        {date_due: '21/08/2018', name: 'Conta de água', value: 50.99, done: 1},
-        {date_due: '22/08/2018', name: 'Conta de Telefone', value: 55.70, done: 1},
-        {date_due: '23/08/2018', name: 'Supermercado', value: 625.00, done: 1},
-        {date_due: '24/08/2018', name: 'Cartão de Créditos', value: 1500.99, done: 1},
-        {date_due: '25/08/2018', name: 'Empréstimos', value: 2000.99, done: 1},
-        {date_due: '26/08/2018', name: 'Gasolina', value: 200, done: 1},
+        {date_due: '21/08/2018', name: 'Conta de água', value: 50.99, done: 0},
+        {date_due: '22/08/2018', name: 'Conta de Telefone', value: 55.70, done: 0},
+        {date_due: '23/08/2018', name: 'Supermercado', value: 625.00, done: 0},
+        {date_due: '24/08/2018', name: 'Cartão de Créditos', value: 1500.99, done: 0},
+        {date_due: '25/08/2018', name: 'Empréstimos', value: 2000.99, done: 0},
+        {date_due: '26/08/2018', name: 'Gasolina', value: 200, done: 0},
     ]
 
   },
@@ -47,6 +49,12 @@ new Vue({
         this.activedView = id;
     }
 
+  },
+
+  watch: {
+      message: function (novoValor,velhoValor) {
+          console.log("velhoValor: "+velhoValor+ ", novoValor: "+novoValor);
+      }
   }
 
 });

57970a42d9fcfdf59ea06ea8e09fd8f9c0728320
Alan Rodrigues <alancrato@hotmail.com>
2018-05-18 09:41:08 -0300

    eventos on-click

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 
 new Vue({
+
   el: '#app',
+
   data: {
+
     title: 'Contas a Pagar',
+
+    menus: [
+        {id: 0, name: 'Contas a Pagar'},
+        {id: 1, name: 'Criar Contas'}
+    ],
+
     bills: [
         {date_due: '20/08/2018', name: 'Conta de Luz', value: 70.99, done: 1},
         {date_due: '21/08/2018', name: 'Conta de água', value: 50.99, done: 1},
@@ -13,8 +22,11 @@ new Vue({
         {date_due: '25/08/2018', name: 'Empréstimos', value: 2000.99, done: 1},
         {date_due: '26/08/2018', name: 'Gasolina', value: 200, done: 1},
     ]
+
   },
+
   computed: {
+
     status: function () {
         var count = 0;
         for(var i in this.bills){
@@ -24,5 +36,15 @@ new Vue({
         }
         return !count?"Nenhuma conta a ser paga":"Existem "+ count+" a serem pagas";
     }
+
+  },
+
+  methods: {
+
+    showView: function (number) {
+        console.log(number);
+    }
+
   }
+
 });
